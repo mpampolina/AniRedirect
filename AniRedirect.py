4fc21775ea19540426a17c3963799f7fcb6641c4
Miguel Pampolina <miguel.r.pampolina@gmail.com>
2020-05-09 17:29:26 -0700

    better link detection, added fallback link

--- AniRedirect.py
+++ AniRedirect.py
@@ -1,15 +1,26 @@
-import pyperclip, animepaheLink, webbrowser
+import pyperclip, animepaheLink, webbrowser, re
 from jikanpy import Jikan
 jikan = Jikan()
 
+animeFallback = 'https://animepahe.com'
+
+MALRegex = re.compile(r'''
+https://myanimelist.net/anime/
+''', re.VERBOSE)
+
 try:
     MALaddress = pyperclip.paste()
-    MALid = MALaddress.split('/')[4]
 
-    search_result = jikan.anime(MALid)
-    title = search_result['title']
+    if bool(MALRegex.search(MALaddress)):   # check if the link is MAL
+        MALid = MALaddress.split('/')[4]
 
-    webbrowser.open(animepaheLink.getAnimePahe(title))
+        search_result = jikan.anime(MALid)
+        title = search_result['title']
+
+        webbrowser.open(animepaheLink.getAnimePahe(title))
+
+    else:
+        webbrowser.open(animeFallback)  # will launch animepahe's homepage if the selected link is invalid
 
 except IndexError as error:
     print('Link is invalid. Please try again.')
